2c38a9849378da41693ab6195ab24baeee286d98
lsheon0927@naver.com <lsheon0927@naver.com>
2024-09-07 11:51:40 +0900

    feat: cors 설정

--- src/main/java/org/example/back/config/interceptor/LogInterceptor.java
+++ src/main/java/org/example/back/config/interceptor/LogInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,6 +20,10 @@ public class LogInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+
+        if (HttpMethod.OPTIONS.matches(request.getMethod())){
+            return true;
+        }
 
         String requestURI = request.getRequestURI();
 
