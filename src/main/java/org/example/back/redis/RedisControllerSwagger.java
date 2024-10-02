a256cd10f26f5a0a7e1b57d3148537850caa6a4f
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-02 13:20:37 +0900

    fix: redis 매핑

--- src/main/java/org/example/back/redis/RedisControllerSwagger.java
+++ src/main/java/org/example/back/redis/RedisControllerSwagger.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "Redis API")
 public interface RedisControllerSwagger {
 
 	@Operation(summary = "Redis 데이터 가져오기", description = "Redis에서 주어진 key에 대한 데이터를 가져옵니다. 만약 해당 key가 존재하지 않으면, 새로운 value를 저장합니다.")
