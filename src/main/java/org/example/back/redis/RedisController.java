284fc28068ad98cd87afaf0e5d8d5c5fe49e1a96
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-02 13:13:15 +0900

    fix: redis 매핑

--- src/main/java/org/example/back/redis/RedisController.java
+++ src/main/java/org/example/back/redis/RedisController.java
@@ -4,6 +4,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 @Tag(name = "Redis", description = "Redis 관련 API")
 public class RedisController implements RedisControllerSwagger {
 
