fe2a0a4ae44ebce12ba2a7c23a2121e4cd8e3da9
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-02 12:50:45 +0900

    fix: redis 매핑 url 추가

--- src/main/java/org/example/back/redis/RedisController.java
+++ src/main/java/org/example/back/redis/RedisController.java
@@ -2,6 +2,7 @@ package org.example.back.redis;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +15,21 @@ public class RedisController implements RedisControllerSwagger {
 	private final RedisService redisService;
 	private final RedisTemplate<String, String> redisTemplate;
 
+	@GetMapping("/redis")
 	@Override
 	public ResponseEntity<String> getRedis(@RequestBody RedisParam param) {
 		String result = redisService.getRedis(param);
 		return ResponseEntity.ok(result);
 	}
 
+	@GetMapping("/manager")
 	@Override
 	public ResponseEntity<String> getRedisWithCacheManager(@RequestBody RedisParam param) {
 		String result = redisService.getRedisWithCacheManager(param);
 		return ResponseEntity.ok(result);
 	}
 
+	@GetMapping("/test-redis")
 	@Override
 	public ResponseEntity<String> testRedisConnection() {
 		try {
