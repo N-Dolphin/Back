91f54f6c038a7e2fc4f7da97d77233ce2749a664
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-09 14:23:44 +0900

    feat: Point 형 좌표를 입력받는 형식
    실제로 쓰이지 않을 수 있음

--- src/main/java/org/example/back/areafilter/AreaFilter.java
+++ src/main/java/org/example/back/areafilter/AreaFilter.java
@@ -1,5 +1,9 @@
 package org.example.back.areafilter;
 
+
+import org.locationtech.jts.geom.Point;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,14 +16,11 @@ public class AreaFilter {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 
-	// profile id;
+	@Column
+	Long profileId;
 
-	String address;
+	@Column(columnDefinition = "geometry(Point,4236)")
+	private Point location;
 
-	Double longitude;
-
-	Double latitude;
-
-	// Postgist point 좌표 4326
 
 }
