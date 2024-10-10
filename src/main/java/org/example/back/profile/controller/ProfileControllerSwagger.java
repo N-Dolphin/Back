d167c7be53a1c588559a3bc9dc0cd8ede4fd5997
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-10 16:27:05 +0900

    refactor: API 문서 수정

--- src/main/java/org/example/back/profile/controller/ProfileControllerSwagger.java
+++ src/main/java/org/example/back/profile/controller/ProfileControllerSwagger.java
@@ -13,7 +13,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Profile API", description = "프로필 관련 API")
 public interface ProfileControllerSwagger {
@@ -70,4 +75,34 @@ public interface ProfileControllerSwagger {
 		}
 	)
 	ResponseEntity<List<Profile>> getDistanceFrom10(HttpServletRequest httpServletRequest);
+
+	@Operation(
+		summary = "프로필 이미지 업로드",
+		description = "사용자의 프로필 이미지를 업로드하고 업로드된 파일의 경로 URL을 반환합니다.",
+		parameters = {
+			@Parameter(name = "profileImage", description = "업로드할 프로필 이미지 파일", required = true, content = @Content(
+				mediaType = "multipart/form-data",
+				schema = @Schema(type = "string", format = "binary", description = "파일 형식"),
+				examples = @ExampleObject(value = "Select files")
+			))
+		},
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "프로필 이미지 업로드 성공",
+				content = @Content(schema = @Schema(type = "string"))
+			),
+			@ApiResponse(
+				responseCode = "400",
+				description = "파일이 없거나 잘못된 요청",
+				content = @Content
+			),
+			@ApiResponse(
+				responseCode = "500",
+				description = "서버 오류로 인한 파일 업로드 실패",
+				content = @Content
+			)
+		}
+	)
+	ResponseEntity<String> uploadProfileImage(@RequestParam("profileImage") MultipartFile file, HttpServletRequest httpServletRequest);
 }
