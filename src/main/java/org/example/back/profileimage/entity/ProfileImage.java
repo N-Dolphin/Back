ebe222bf11ad1cff0855bd8cac26a9c58d7a00f2
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-14 19:04:18 +0900

    refactor: 프로필 이미지 로직 수정
    DTO 수정 및 리턴값 수정

--- src/main/java/org/example/back/profileimage/entity/ProfileImage.java
+++ src/main/java/org/example/back/profileimage/entity/ProfileImage.java
@@ -1,11 +1,20 @@
 package org.example.back.profileimage;
 
+import org.example.back.profile.domain.Profile;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+
 public class ProfileImage {
 
 	@Id
@@ -13,6 +22,10 @@ public class ProfileImage {
 	Long id;
 
 	// 프로필 아이디
+	// 프로필과의 연관관계 (다대일)
+	@ManyToOne
+	@JoinColumn(name = "profile_id")
+	private Profile profile;
 
 	String imageUrl;
 
