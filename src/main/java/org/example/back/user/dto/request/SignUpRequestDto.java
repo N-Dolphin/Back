26b196d6767d80e8ca36abbcd8f1cb0979669155
lsheon0927@naver.com <lsheon0927@naver.com>
2024-10-17 16:22:14 +0900

    refactor: 일반 로그인 프로퍼티 통일

--- src/main/java/org/example/back/user/dto/request/SignUpRequestDto.java
+++ src/main/java/org/example/back/user/dto/request/SignUpRequestDto.java
@@ -7,12 +7,12 @@ import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequestDto(
 
+	@Schema(description = "이메일 주소", example = "lsh0927@naver.com")
+	@NotBlank @Email String email,
+
 	@Schema(description = "규격에 맞는 비밀번호", example = "password1234")
 	@NotBlank @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,13}$")
 	String password,
-
-	@Schema(description = "이메일 주소", example = "lsh0927@naver.com")
-	@NotBlank @Email String email,
 
 	@Schema(description = "이메일 인증 코드", example = "1234")
 	@NotBlank String certificationNumber
