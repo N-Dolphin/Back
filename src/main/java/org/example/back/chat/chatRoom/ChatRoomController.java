277a2a7254ea55fe120432a7cd56a8e77cdb73b8
lsheon0927@naver.com <lsheon0927@naver.com>
2024-11-24 22:39:27 +0900

    fix
    url 누락

--- src/main/java/org/example/back/chat/chatRoom/ChatRoomController.java
+++ src/main/java/org/example/back/chat/chatRoom/ChatRoomController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("api/v1")
 class ChatRoomController implements  ChatRoomControllerSwagger{
 
 	private final ChatRoomServiceImpl chatRoomService;
