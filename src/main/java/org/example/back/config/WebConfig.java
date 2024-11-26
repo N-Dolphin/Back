3acb4739dd2841dcfa6f01f45d9c97f7ba23b4f2
lsheon0927@naver.com <lsheon0927@naver.com>
2024-11-26 14:09:45 +0900

    fix: Cors 에러

--- src/main/java/org/example/back/config/WebConfig.java
+++ src/main/java/org/example/back/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
@@ -42,7 +43,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")  // Vite 개발 서버 주소
+            .allowedOrigins("*")  // 모든 출처 허용
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("Authorization")  // Authorization 헤더 노출
