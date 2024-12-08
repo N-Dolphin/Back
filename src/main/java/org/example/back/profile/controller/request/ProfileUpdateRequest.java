338095567559a85c73a9dee4d808d9419883e96c
lsheon0927@naver.com <lsheon0927@naver.com>
2024-12-08 14:35:48 +0900

    feat: 프로필 수정 기능
    이름과 자기소개만 변경할 수 있도록 변경

---
+++ src/main/java/org/example/back/profile/controller/request/ProfileUpdateRequest.java
@@ -0,0 +1,32 @@
+package org.example.back.profile.controller.request;
+
+import java.time.LocalDate;
+
+import org.example.back.profile.domain.Profile;
+import org.example.back.profile.domain.type.Gender;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Schema(name = "프로필 생성 Request")
+public record ProfileUpdateRequest(
+
+	@Schema(description = "유저명", example = "피카츄")
+	@NotBlank
+	String profileName,
+
+	@Schema(description = "자기소개", example = "안녕하세요")
+	@NotBlank
+	String selfIntroduction
+
+
+) {
+	public Profile toUpdateProfile(Profile profile) {
+		return Profile.builder()
+			.profileName(this.profileName)
+			.selfIntroduce(this.selfIntroduction)
+			.build();
+	}
+}
