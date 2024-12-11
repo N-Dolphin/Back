6dd7500de9c4bfceb589588e88040495dcd9fbf6
lsheon0927@naver.com <lsheon0927@naver.com>
2024-12-11 15:39:05 +0900

    fix: cors 주석처리

--- src/main/java/org/example/back/config/WebConfig.java
+++ src/main/java/org/example/back/config/WebConfig.java
@@ -43,12 +43,12 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("*")
-            .allowedHeaders("*")
-            .exposedHeaders("Authorization");
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //         .allowedOrigins("http://localhost:3000")
+    //         .allowedMethods("*")
+    //         .allowedHeaders("*")
+    //         .exposedHeaders("Authorization");
+    // }
 }
