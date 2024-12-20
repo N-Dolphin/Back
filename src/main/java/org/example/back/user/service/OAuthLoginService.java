ccffb615efb55e849425c8d5035ba608343f8165
lsheon0927@naver.com <lsheon0927@naver.com>
2024-12-20 15:40:25 +0900

    HotFix: 기존 유저 체크 로직 수정
    카카오 로그인에서 기존 유저 확인 못하는 버그 수정

--- src/main/java/org/example/back/user/service/OAuthLoginService.java
+++ src/main/java/org/example/back/user/service/OAuthLoginService.java
@@ -2,7 +2,6 @@ package org.example.back.user.service;
 
 import java.util.Optional;
 
-import org.example.back.config.provider.AuthTokens;
 import org.example.back.config.provider.AuthTokensGenerator;
 import org.example.back.profile.domain.Profile;
 import org.example.back.profile.repository.ProfileRepository;
@@ -27,24 +26,32 @@ public class OAuthLoginService {
 	private final ProfileRepository profileRepository;
 	private final ProfileImageRepository profileImageRepository;
 
+	@Transactional
 	public SignInResponseDto login(OAuthLoginParams params) {
 		OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
-		UserEntity userEntity = findOrCreateMember(oAuthInfoResponse);
 
-		Optional<Profile> profile = profileRepository.findByUserId(userEntity.getUserId());
+		Optional<UserEntity> userEntity= Optional.empty();
+
+		if (userRepository.findByEmail(oAuthInfoResponse.getEmail()).isPresent()){
+			userEntity= userRepository.findByEmail(oAuthInfoResponse.getEmail());
+		}
+		else {
+			userEntity= Optional.ofNullable(findOrCreateMember(oAuthInfoResponse));
+		}
+
+		Optional<Profile> profile = profileRepository.findByUserId(userEntity.get().getUserId());
 		boolean hasProfile = profile.isPresent();
 		boolean hasProfileLocation = profile.map(Profile::getLocation).isPresent();
 		boolean hasProfileImage = profile.map(Profile::getProfileId)
 			.flatMap(profileImageRepository::findFirstByProfile_ProfileId) // 여기 수정
 			.isPresent();
 
-		return new SignInResponseDto(authTokensGenerator.generate(userEntity.getUserId()),3600L, hasProfile,hasProfileImage,hasProfileLocation);
+		return new SignInResponseDto(authTokensGenerator.generate(userEntity.get().getUserId()),3600L, hasProfile,hasProfileImage,hasProfileLocation);
 	}
 
+	@Transactional
 	public UserEntity findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
-		return userRepository.findByEmail(oAuthInfoResponse.getEmail()).orElse(
-			newMember(oAuthInfoResponse)
-		);
+		return newMember(oAuthInfoResponse);
 	}
 
 	@Transactional
